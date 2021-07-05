bfb3fa03a5a9da6a108949ddc81194603061365b
cengz <cengzdenz@hotmail.com>
2021-07-05 19:09:15 +0000

    ilk ödev

---
+++ cengiz.py
@@ -0,0 +1,6 @@
+import json
+dosya = open ("cengiz.json", "r")
+json_dosya = json.load(dosya)
+siralanacak=json_dosya["kimlik"]
+print(json.dumps(siralanacak,indent=2,sort_keys=True))
+print(json_dosya["kimlik"])
